de60a65a1da14c1f1d679661ad89a320fa8dc099
Yaswanth Kumar <124058908+yaswanth0091@users.noreply.github.com>
2024-08-12 23:34:21 +0530

    Time: 1 ms (79.46%), Space: 40.7 MB (68.06%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -7,14 +7,12 @@ class Solution {
         int t=x*a;
         while(t>0){
             int d=t%10;
-            if (sum > (Integer.MAX_VALUE - d) / 10) {
+            if (sum>(Integer.MAX_VALUE-d)/10){
                 return 0;
             }
-
-            sum = sum * 10 + d;
-            t = t / 10;
+            sum=sum*10+d;
+            t=t/10;
         }
-
-        return sum * a;
+        return sum*a;
     }
 }
